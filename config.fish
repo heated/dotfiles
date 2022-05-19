98b9f26696a3b6edab70203b8ea3661fdd1d4d75
Edward Swernofsky <edabsw@gmail.com>
2022-05-19 14:19:25 -0400

    add personal dotfiles

---
+++ config.fish
@@ -0,0 +1,14 @@
+function fish_greeting
+end
+
+fish_add_path /opt/local/bin
+fish_add_path /opt/local/sbin 
+fish_add_path /Users/edward/bin 
+fish_add_path /Users/edward/.cargo/bin 
+fish_add_path /usr/local/sbin
+fish_add_path /usr/local/opt/ruby/bin
+fish_add_path /usr/local/lib/ruby/gems/3.1.0/bin
+fish_add_path /usr/local/opt/python@3.9/Frameworks/Python.framework/Versions/3.9/bin
+
+set -x GOPATH ~/projects/go
+set -x HOMEBREW_NO_AUTO_UPDATE 1
